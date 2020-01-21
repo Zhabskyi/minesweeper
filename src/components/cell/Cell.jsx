d6b77413d164994b40abc941a952de466c4d1f3c
Zhabskyi <poltavagaz@gmail.com>
2020-01-21 14:15:18 -0700

    Set numbers around bombs

--- src/components/cell/Cell.jsx
+++ src/components/cell/Cell.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import classes from "./Cell.module.scss";
 
 const Cell = ({ item }) => {
-
   if (item === 1) {
-    
+    return <div className={classes.bomb}></div>;
+  } else {
+    return <div className={classes.container}></div>;
   }
-  return <div className={classes.container}></div>;
 };
 
 export default Cell;
--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -16,6 +16,65 @@ const Grid = () => {
 
   console.log(gridOfBombs);
 
+  const setCellNumberForBombs = gridOfBombs => {
+    for (let i = 0; i < gridOfBombs.length; i++) {
+      for (let j = 0; j < gridOfBombs[i].length; j++) {
+        if (gridOfBombs[i][j] === 1) {
+          if (j - 1 >= 0) {
+            if (gridOfBombs[i][j - 1] !== 1) {
+              gridOfBombs[i][j - 1] += 2;
+            }
+          }
+
+          if (j + 1 < gridOfBombs.length) {
+            if (gridOfBombs[i][j + 1] !== 1) {
+              gridOfBombs[i][j + 1] += 2;
+            }
+          }
+
+          if (j - 1 >= 0 && i - 1 >= 0) {
+            if (gridOfBombs[i - 1][j - 1] !== 1) {
+              gridOfBombs[i - 1][j - 1] += 2;
+            }
+          }
+
+          if (i - 1 >= 0) {
+            if (gridOfBombs[i - 1][j] !== 1) {
+              gridOfBombs[i - 1][j] += 2;
+            }
+          }
+
+          if (i - 1 >= 0 && j + 1 < gridOfBombs.length) {
+            if (gridOfBombs[i - 1][j + 1] !== 1) {
+              gridOfBombs[i - 1][j + 1] += 2;
+            }
+          }
+
+          if (i + 1 < gridOfBombs.length && j - 1 >= 0) {
+            if (gridOfBombs[i + 1][j - 1] !== 1) {
+              gridOfBombs[i + 1][j - 1] += 2;
+            }
+          }
+
+          if (i + 1 < gridOfBombs.length) {
+            if (gridOfBombs[i + 1][j] !== 1) {
+              gridOfBombs[i + 1][j] += 2;
+            }
+          }
+
+          if (i + 1 < gridOfBombs.length && j + 1 < gridOfBombs.length) {
+            if (gridOfBombs[i + 1][j + 1] !== 1) {
+              gridOfBombs[i + 1][j + 1] += 2;
+            }
+          }
+        }
+      }
+    }
+    return gridOfBombs;
+  };
+
+  console.log(setCellNumberForBombs(gridOfBombs));
+
   return (
     <div className={classes.container}>
       {gridOfBombs.map(el => el.map(item => <Cell item={item} />))}
