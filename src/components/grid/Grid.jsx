6a2b9698729143cd240d644d4b122459a83b9a44
Zhabskyi <poltavagaz@gmail.com>
2020-01-22 00:36:41 -0700

    Delete mixins and grid from styles

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -5,16 +5,18 @@ import Cell from "../../containers/Cell";
 const Grid = props => {
   useEffect(() => {
     props.loadCells();
-    console.log(props);
     // eslint-disable-next-line
   }, []);
 
   let result = [];
+  let key = 0;
   if (props.cells) {
     for (let i = 0; i < props.cells.length; i++) {
       for (let j = 0; j < props.cells[i].length; j++) {
+        key++;
         result.push(
           <Cell
+            key={key}
             coordinates={[i, j]}
             open={props.isCellOpen[i][j]}
             flag={props.isFlag[i][j]}
